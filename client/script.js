c7c928ccdeae5528d468526783597177bdfd98a1
mistlady <127565435+mistlady@users.noreply.github.com>
2023-09-13 16:27:34 +0100

    Add files via upload

---
+++ client/script.js
@@ -0,0 +1,43 @@
+const game = document.querySelectorAll(".game");
+const question = game[0];
+const answer = game[1];
+
+const createTicketDomQUS = (id, qus) => {
+  const ticket = document.createElement("div");
+  ticket.classList.add("ticket");
+  ticket.id = id;
+  ticket.innerHTML = `<h1>${qus}</h1>`;
+  return ticket;
+};
+
+const createTicketDomANS = (id, ans) => {
+  const ticket = document.createElement("div");
+  ticket.classList.add("ticket");
+  ticket.id = id;
+  ticket.innerHTML = `<p>B-${ans}</p>`;
+  return ticket;
+};
+
+// http://localhost:8080/api/level1
+
+let FirstQus = [];
+let FirstAns = [];
+(async function () {
+  try {
+    const postqus = await fetch("http://localhost:8080/api/level1");
+    FirstQus = await postqus.json();
+
+    const postans = await fetch("http://localhost:8080/api/level1");
+    FirstAns = await postans.json();
+
+    FirstQus.data.forEach((task) => {
+      question.appendChild(createTicketDomQUS(task.id, task.qus));
+    });
+
+    FirstAns.data.forEach((task) => {
+      answer.appendChild(createTicketDomANS(task.id, task.ans));
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+})(FirstQus, FirstAns, createTicketDomQUS, createTicketDomANS);
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const db = fs.readFileSync(path.resolve(__dirname, "db.json"));
+const data = JSON.parse(db);
+
+app.get("/api", (req, res) => {
+  return res.status(200).json({ data: "hello world" });
+});
+
+app.get("/api/level1", (req, res) => {
+  return res.status(200).json({ data: data.level1 });
+});
+
+app.get("/api/level2", (req, res) => {
+  return res.status(200).json({ data: data.level2 });
+});
+
+app.get("/api/level3", (req, res) => {
+  return res.status(200).json({ data: data.level3 });
+});
+
+app.get("/api/level4", (req, res) => {
+  return res.status(200).json({ data: data.level4 });
+});
+
+app.get("/api/level5", (req, res) => {
+  return res.status(200).json({ data: data.level5 });
+});
+
+app.listen(port, () => {
+  console.log(`Server run on port ${port}`);
+});
