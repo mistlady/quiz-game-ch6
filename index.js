1ffc5803a20c406f16380497bde60d6f598b5f92
mistlady <127565435+mistlady@users.noreply.github.com>
2023-09-15 20:11:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
